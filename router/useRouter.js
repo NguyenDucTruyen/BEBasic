c3dc46806dddd54078104e88e26bded6a95f9840
DucTruyen <mrtruyenbd1407@gmail.com>
2023-04-13 18:31:01 +0700

    upload

--- router/useRouter.js
+++ router/useRouter.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-
+const connection = require('../database/connection')
 let maxId = 0;
 
 
@@ -55,42 +55,53 @@ let list=[
 ]
 
 //Get methods
-router.get('/', (req, res) => {return res.send(list)})
+router.get('/', (req, res) => {
+	let arrayUser=[] 
+	connection.query('select * from user',(err,rs)=>{
+		if(err) throw err;
+		return res.send(rs)
+		
+	})
+
+})
 
 // Tìm kiếm user theo id
 router.get('/:id',function (req,res){
-	const id = parseInt(req.params.id); // chuyển đổi id thành kiểu số nguyên
-	const user = list.find(userTemp => userTemp.id === id); // tìm kiếm người dùng theo id
-	if (!user) {
-	  return res.status(404).send('Không thể tìm thấy'); // trả về mã trạng thái 404 nếu không tìm thấy người dùng
-	}
-	else{
-		res.send(user); // trả về thông tin người dùng
-	}
+	connection.query('select * from user',(err,rs)=>{
+		if(err) throw err;
+		const id = parseInt(req.params.id); // chuyển đổi id thành kiểu số nguyên
+		const user = rs.find(userTemp => userTemp.id === id); // tìm kiếm người dùng theo id
+		if (!user) {
+		return res.status(404).send('Không thể tìm thấy'); // trả về mã trạng thái 404 nếu không tìm thấy người dùng
+		}
+		else{
+			return res.send(user); // trả về thông tin người dùng
+		}
+	})
+	
 })
 
 
 //Put methods
 router.put('/:id',Validate,(req,res)=>{
 	const id = parseInt(req.params.id)
-	const user = list.find(user => user.id === id)
-	if(!user){
-		return res.status(404).send('Không thể tìm thấy'); 	
-	}
-	else{
-		user.fullname = req.body.fullname
-		user.gender = (Boolean) (req.body.gender)
-		user.age = parseInt(req.body.age)
-		res.status(204).send("ok")
-	}
+	let user={}
+	user.fullname = req.body.fullname
+	user.gender = (Boolean) (req.body.gender)
+	user.age = parseInt(req.body.age)
+	connection.query('update user set fullname=?,gender=?,age=? where id =?',[user.fullname,user.gender,user.age,id])
+	res.status(204).send("ok")
+	
 })
+
 
 // Post methods
 	router.post('/',Validate,(req,res)=>{
 		const user = req.body
-		
-		user.id =getMaxID(list)+1
-		list.push(user)
+		connection.query('insert into user(fullname,gender,age)'+
+		'values(?,?,?)',[user.fullname,Boolean(user.gender),parseInt(user.age)],(err, rs)=>{
+			console.log(err)
+})
 		res.status(201).send(user)
 		
 	})
@@ -98,14 +109,22 @@ router.put('/:id',Validate,(req,res)=>{
 // Delete methods
 	router.delete('/:id',(req,res)=>{
 		const id = parseInt(req.params.id)
-		const index = list.findIndex(user => user.id === id)
-		if(index !==-1){
-			list.splice(index,1)
-            res.status(204).send("Deleted")
-		}
-		else{
-			res.status(404).send("Không thể tìm thấy")
-		}
+		connection.query('select * from user where id =?',id,(err,rs)=>{
+			if(err) res.send(err)
+			else
+				if(!rs)
+				res.status(404).send("Không thể tìm thấy")
+				else
+					{
+						connection.query('delete from user where id =?',id,(err,rs)=>{
+							if(err) res.send(err)
+							else
+							res.status(204).send("Deleted")
+						})	
+					}
+		})
+		
+		
 	})
 
     module.exports = router
--- test.js
+++
@@ -1,6 +0,0 @@
-let connection = require('./database/connection')
-
-connection.query("select * from user",(err,rs)=>{
-    console.log(err)
-    console.log(rs)
-})
