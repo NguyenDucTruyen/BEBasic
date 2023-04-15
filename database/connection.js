3f37fe9d962b5369946468c0181c8fcbd2ed6c50
DucTruyen <mrtruyenbd1407@gmail.com>
2023-04-15 23:24:59 +0700

    Buoi 4 - pagkage dotenv

---
+++ database/connection.js
@@ -0,0 +1,11 @@
+let mysql = require("mysql");
+let dotenv = require('dotenv')
+dotenv.config()
+
+let connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database:process.env.DB_NAME,
+});
+module.exports = connection
