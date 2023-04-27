1b9d838fb2dbbd8e370485d3fa784b3602180768
DucTruyen <mrtruyenbd1407@gmail.com>
2023-04-27 19:48:02 +0700

    upload lan2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const useRouter = require('./router/auth')
+const authRoute = require('./router/auth')
 
 var bodyParser = require('body-parser') 
 app.use(bodyParser.urlencoded({ extended: false })) 		// parse application/x-www-form-urlencoded
@@ -17,7 +17,7 @@ app.use(bodyParser.json())	                            // parse application/json
     ---> dùng app.use(......) để tất cả route đều có thể sử dụng
 */
 
-app.use('/user',useRouter)
+app.use('/auth',authRoute)
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
--- router/auth.js
+++ router/auth.js
@@ -3,60 +3,74 @@ const router = express.Router()
 const connection = require('../database/connection')
 const query = require('../database/query')
 
-const insert ="insert into user(username,password,name,age,gender,email) values(?,?,?,?,?,?)"
-let maxId = 0;
+const insert = "insert into user(username,password,salt,name,age,gender,email) values(?,?,?,?,?,?,?)"
+//vALIDATE
+const validateRegisterRequest = (req, res, next) => {
+    user = {
+        username: req.body.username,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        email: req.body.email,
+        gender: req.body.gender,
+        name: req.body.name,
+        age: parseInt(req.body.age),
+    }
+    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+    if (user.age <= 0) {
+        res.status(400).json({ message: 'age not valid' })
+    }
+    else if (user.username.length <= 3 || user.password.length <= 3) {
+        res.status(400).json({ message: 'username or password not valid' })
+    }
+    else if (user.password !== user.confirmPassword) {
+        res.status(400).json({ message: '2 password not same' })
+    }
+    else if (!emailRegex.test(user.email)) {
+        res.status(400).json({ message: 'email not valid' })
+    }
+    else if (user.name.length < 2) {
+        res.status(400).json({ message: 'name not valid' })
+    }
+    else {
+        next()	;
+    }
 
-
-
-  function fullNameValidate(name) {
-	const pattern = /^[a-zA-ZÀ-ỹạẠảẢấẤầẦẩẨẫẪậẬắẮằẰẳẲẵẴặẶéÉèÈẻẺẽẼẹẸêÊếẾềỀểỂễỄệỆíÍìÌỉỈĩĨ//ịỊóÓòÒỏỎõÕọỌôÔốỐồỒổỔỗỖộỘơƠớỚờỜởỞỡỠợỢúÚùÙủỦũŨụỤưỨỨừỪửỬữỮựỰýÝỳỲỷỶỹỸ\s]+$/;
-	if (!name || name.length < 2 || name.length > 50) {
-	  	return false 		// Độ dài tên có vấn đề
-	}
-	else if(!pattern.test(name)){
-		return false;
-	}
-	return true;
-  }
-  function validateEmail(email) {
-	const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-	return re.test(email);
-  }
-function Validate(req,res,next){
-
-	if(!fullNameValidate(req.body.username)){
-		res.status(204).send("Tên không hợp lệ");
-	}
-	else if(req.body.password != req.body.passwordagain){
-		res.status(204).send("Mật khẩu nhập lại không đúng");
-	}
-	else if(req.body.gender !='1' && req.body.gender !='0'){
-		res.status(204).send("Giới tính hợp lệ")
-	}
-	else if(req.body.age < 0 || req.body.age >120)
-	{
-		res.status(204).send("Độ tuổi không hợp lệ")
-	}
-	else if(!fullNameValidate(req.body.name)){
-		res.status(204).send("Tên không hợp lệ");
-	}
-	else if(!validateEmail(req.body.email)){
-		res.status(204).send("Email không hợp lệ");
-	};
-
-	next();
-	
 }
+//End validate
+
+const { hashPassword,
+	comparePassword } = require('../helper/hash');
 // Register user
-router.post('/register',Validate,async (req,res)=>{
-	const user = req.body;
-	await query.create({
-		db:connection,
-		query:insert,
-		params:[user.username,user.password,user.name,user.age,user.gender,user.email]
+router.post('/register', validateRegisterRequest, async (req, res) => {
+	let user = {};
+	user.username = req.body.username;
+	user.name = req.body.name;
+	user.age = req.body.age;
+	user.email = req.body.email;
+	user.gender = req.body.gender;
+	const userExisted = await query.getOne({
+		db: connection,
+		query: 'select * from user where username =?',
+		params: user.username
 	});
-	
+	if (userExisted) {
+		res.status(400).json("Existed user");
+	}
+	else {
+		const passWord_salt = hashPassword(req.body.password);
+		user.password = passWord_salt.hashedPassword;
+		user.salt = passWord_salt.salt;
+
+		await query.create({
+			db: connection,
+			query: insert,
+			params: [user.username, user.password, user.salt, user.name, user.age, user.gender, user.email]
+		});
+		res.status(200).json(user);
+	}
+
 })
+
 
 //Get methods
 router.get('/', (req, res) => {
@@ -64,7 +78,7 @@ router.get('/', (req, res) => {
 	connection.query('select * from user',(err,rs)=>{
 		if(err) throw err;
 		return res.send(rs)
-		
+
 	})
 
 })
@@ -82,7 +96,7 @@ router.get('/:id',function (req,res){
 			return res.send(user); // trả về thông tin người dùng
 		}
 	})
-	
+
 })
 
 
@@ -95,7 +109,7 @@ router.put('/:id',Validate,(req,res)=>{
 	user.age = parseInt(req.body.age)
 	connection.query('update user set fullname=?,gender=?,age=? where id =?',[user.fullname,user.gender,user.age,id])
 	res.status(204).send("ok")
-	
+
 })
 
 
@@ -107,7 +121,7 @@ router.put('/:id',Validate,(req,res)=>{
 			console.log(err)
 		})
 		res.status(201).send(user)
-		
+
 	})
 
 // Delete methods
@@ -127,10 +141,9 @@ router.put('/:id',Validate,(req,res)=>{
 						})	
 					}
 		})
-		
-		
+
+
 	})
 
-	// router.post('/register',)
 
-    module.exports = router
+module.exports = router
